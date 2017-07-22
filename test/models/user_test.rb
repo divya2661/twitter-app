73beda60a3413c4024b4606adcfce5f07862e341
divya.n <divya.n@go-jek.com>
2017-07-22 18:56:47 +0530

    Can validate nil user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: 'test user', email: 'divn2661@gmail.com', password: '12345')
+  end
+
+  test 'user should be valid' do
+    assert @user.valid?
+  end
+
 end
