b7ce5a3438dd31624281203ded564ea1a645134a
divya.n <divya.n@go-jek.com>
2017-07-23 00:38:53 +0530

    Added empty session controlled to store sessions for logged in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users
   root 'application#hello'
   get  '/signup',  to: 'users#new'
   get  '/signup',  to: 'users#create'
+  get  '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
