7eb7479540a534a255b9f3f639d7dd82a7791cd3
divya.n <divya.n@go-jek.com>
2017-07-24 01:52:52 +0530

    Integration test for redirect flow

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -10,11 +10,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test 'login with invalid information' do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: {session: {email: "", password: ""}}
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
   end
 
+  test 'login with valid information' do
+    get login_path
+    post login_path, params: {session: {email: @user.email, password: '1234'}}
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+  end
 end
