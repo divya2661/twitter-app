cb4e244e8fd4553c0666ba556e709843f0fa7555
divya.n <divya.n@go-jek.com>
2017-07-23 20:58:21 +0530

    Removed password confirmation field from signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
   validates :password, presence: true
-
 end
